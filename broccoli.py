caf17226daef701e66b202e5b81806d6fdd5453c
Chi Zhang <cz1616@sprite38.doc.ic.ac.uk>
2019-01-24 18:42:29 +0000

    Implement decision_tree_learning function, construct the decision tree.

--- broccoli.py
+++ broccoli.py
@@ -12,32 +12,60 @@ class TreeNode:
         self.lChild = lc
         self.rChild = rc
 
-# def decision_tree_learning(dataset, depth):
-#     if len(set([data[-1] for data in dataset])) == 1:         #check the last column(labels) and if all of them are samely labeled, return this dataset as a leaf
-#         return (TreeNode((7,dataset[0][7]), None, None), depth)
-#     else:
-#
-#
-# def find_split(dataset):
-#     for column in range(7):
-#         temp = list(dataset)
-#         dataset = np.array(sorted(temp,key=lambda x:x[column])) #Sort the dataset w.r.t the column's value of every datum.
-#         biggest_gain = 0 #Store the biggest gain.
-#         for num in range(2000):
-#             if dataset[num][7] != dataset[num+1][7]: #Find a split point.
-#                 set_left =
+def decision_tree_learning(dataset, depth):
+    if len(set([data[-1] for data in dataset])) == 1:         #check the last column(labels) and if all of them are samely labeled, return this dataset as a leaf
+        return (TreeNode((7,dataset[0][7]), None, None), depth)
+    else:
+        node = find_split(dataset)
+        temp = list(dataset)
+        dataset = np.array(sorted(temp, key=lambda x:(x[node[0]], x[7]))) #Sort the dataset w.r.t the column's value of every datum.
+        (l_dataset, r_dataset) = split_set(dataset, node[1], node[0])
+        (l_branch, l_depth) = decision_tree_learning(l_dataset, depth + 1)
+        (r_branch, r_depth) = decision_tree_learning(r_dataset, depth + 1)
+        return (TreeNode(node, l_branch, r_branch), max(l_depth, r_depth))
 
-def split_set(data, split_point):
+
+def find_split(dataset): #returns a treeNode with the splitting column and the splitting point stored
+    column_length = len(dataset[0])
+    max_gain = 0
+    max_gain_split_value = 0
+    column_no = 0;
+    for column in range(column_length):
+         (max_gain_col, max_gain_split_point_col) = find_split_point_for_column(dataset, column)
+         if (max_gain_col > max_gain):
+             max_gain = max_gain_col
+             max_gain_split_value = max_gain_split_point_col
+             column_no = column
+    return (column_no, max_gain_split_value)
+
+def find_split_point_for_column(dataset, column): #return a best gaining splitting point and its gain in tuples
+    temp = list(dataset)
+    dataset = np.array(sorted(temp, key=lambda x:(x[column], x[7]))) #Sort the dataset w.r.t the column's value of every datum.
+    biggest_gain = 0 #Store the biggest gain.
+    biggest_gain_splitting_point = 0
+    row_length = len(dataset)
+    for num in range(row_length):
+        if (dataset[num][7] != dataset[num+1][7]) && (dataset[num][column] != dataset[num+1][column]): #Find a split point.
+            split_value = dataset[num+1][column]
+            (set_left, set_right) = split_set(dataset, split_value, column)
+            gain = calculate_gain(dataset, set_left, set_right)
+            if biggest_gain < gain:
+                biggest_gain = gain
+                biggest_gain_splitting_point = split_value
+
+    return (biggest_gain, biggest_gain_splitting_point)
+
+def split_set(data, split_value, column):
     set_left = []
     set_right = []
     for datum in data:
-        if datum < split_point:
+        if datum[column] < split_value:
             set_left.append(datum)
         else:
             set_right.append(datum)
     return (set_left, set_right)
 
-def calculate_gain(dataset, left, right, column):
+def calculate_gain(dataset, left, right):
     left_size = len(left)
     right_size = len(right)
     remainder = left_size/len(dataset) * calculate_enthropy(left) + right_size/len(dataset) * calculate_enthropy(right)
